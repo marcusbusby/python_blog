1086d5d34d459a1601dbfffc9cbfe733201b83b6
Marcus Busby <marcusdbusby@gmail.com>
2015-06-05 02:38:41 +0000

    adding testing

---
+++ tests/test_filter.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+import datetime
+
+# Configure your app to use the testing configuration
+if not "CONFIG_PATH" in os.environ:
+    os.environ["CONFIG_PATH"] = "blog.config.TestingConfig"
+
+import blog
+from blog.filters import *
+
+class FilterTests(unittest.TestCase):
+    def testDateFormat(self):
+        # Tonight we're gonna party...
+        date = datetime.date(1999, 12, 31)
+        formatted = dateformat(date, "%y/%m/%d")
+        self.assertEqual(formatted, "99/12/31")
+
+    def testDateFormatNone(self):
+        formatted = dateformat(None, "%y/%m/%d")
+        self.assertEqual(formatted, None)
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
